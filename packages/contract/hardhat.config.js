dbcbfa1f13a1413e672532819e04a3170e3a9838
“niconiconainu” <kyosuke.soccer118@gmail.com>
2024-08-30 16:50:54 +0900

    done section1

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -6,8 +6,10 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     sepolia: {
-      url: "YOUR_ALCHEMY_API_URL" || "",
-      accounts: "YOUR_PRIVATE_ACCOUNT_KEY" ? ["YOUR_PRIVATE_ACCOUNT_KEY"] : [],
+      url: "https://eth-sepolia.g.alchemy.com/v2/Xj-41rkIzeKAQ36ST1JjkYmVziUIzY51" || "",
+      accounts: "81169f1eb8079e07135711eefebeec27f68df01de632ed7691c716952b66e825" ? ["81169f1eb8079e07135711eefebeec27f68df01de632ed7691c716952b66e825"] : [],
+      // url: "YOUR_ALCHEMY_API_URL" || "",
+      // accounts: "YOUR_PRIVATE_ACCOUNT_KEY" ? ["YOUR_PRIVATE_ACCOUNT_KEY"] : [],
     },
   },
 };
